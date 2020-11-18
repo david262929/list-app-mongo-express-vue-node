42690ff5b9f7cef5918c6ba1c0b196d3b9cae8a2
David <davidghazaryan2000@gmail.com>
2020-11-18 20:53:02 +0400

    implemented mongoose connect, morgan(logger),

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
+const mongoose = require('mongoose')
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const fs = require('fs')
+const bodyParser = require('body-parser')
+
 const app = express()
-const PORT = 5003
+
+const {MONGODB_URI, PORT} = require('./configs')
+
+app.use(cors())
+app.use(morgan(`:method :url :status :res[content-length] - :response-time ms `, {
+  stream: fs.createWriteStream('./logs/access.log', {flags: 'a'}),
+}))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.status(200).send('Hello world'))
 
-app.listen(PORT, () => console.log(`App started on port:${PORT}`))
+app.listen(PORT, async () => {
+  console.log(`App started on port:${PORT}`)
+
+  try{
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      autoIndex: false,
+    })
+    console.log('successfully connected to mongodb')
+  }catch (e) {
+    console.log('Have a some error -_-_-_- ', e)
+  }
+})
