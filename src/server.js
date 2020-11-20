d0657896b5918ca40d1e157797a12ff15c40f0f3
David <davidghazaryan2000@gmail.com>
2020-11-20 20:47:03 +0400

    some tests with other routes

--- src/server.js
+++ src/server.js
@@ -35,9 +35,10 @@ if(NODE_ENV === 'development'){
   app.get('/', (req, res) => res.status(200).send('Hello world'))
 }else if(NODE_ENV === 'production'){
   app.use(express.static('client/dist'))
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
   })
+  app.get('/davoo', (req, res) => res.status(200).send('davoo'))
 }
 
 
